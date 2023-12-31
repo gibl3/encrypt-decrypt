ea42c65852abfed2a17103dc812a7d2ac8633a60
Noceur <bernardglenn.casera@evsu.edu.ph>
2023-12-31 09:39:19 +0800

    replaced the classmethods

--- morse.py
+++ morse.py
@@ -1,158 +1,167 @@
-class MorseCodeError(Exception):
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
-
-
 class Morse:
-    morse_chart = {
-        "A": ".-",
-        "B": "-...",
-        "C": "-.-.",
-        "D": "-..",
-        "E": ".",
-        "F": "..-.",
-        "G": "--.",
-        "H": "....",
-        "I": "..",
-        "J": ".---",
-        "K": "-.-",
-        "L": ".-..",
-        "M": "--",
-        "N": "-.",
-        "O": "---",
-        "P": ".--.",
-        "Q": "--.-",
-        "R": ".-.",
-        "S": "...",
-        "T": "-",
-        "U": "..-",
-        "V": "...-",
-        "W": ".--",
-        "X": "-..-",
-        "Y": "-.--",
-        "Z": "--..",
-        "1": ".----",
-        "2": "..---",
-        "3": "...--",
-        "4": "....-",
-        "5": ".....",
-        "6": "-....",
-        "7": "--...",
-        "8": "---..",
-        "9": "----.",
-        "0": "-----",
-        ", ": "--..--",
-        ".": ".-.-.-",
-        "?": "..--..",
-        "!": "-.-.--",
-        "/": "-..-.",
-        "-": "-....-",
-        "(": "-.--.",
-        ")": "-.--.-",
-    }
-    characters = list(morse_chart.keys())
-    codes = list(morse_chart.values())
+    def __init__(self):
+        self._morse_chart = {
+            "A": ".-",
+            "B": "-...",
+            "C": "-.-.",
+            "D": "-..",
+            "E": ".",
+            "F": "..-.",
+            "G": "--.",
+            "H": "....",
+            "I": "..",
+            "J": ".---",
+            "K": "-.-",
+            "L": ".-..",
+            "M": "--",
+            "N": "-.",
+            "O": "---",
+            "P": ".--.",
+            "Q": "--.-",
+            "R": ".-.",
+            "S": "...",
+            "T": "-",
+            "U": "..-",
+            "V": "...-",
+            "W": ".--",
+            "X": "-..-",
+            "Y": "-.--",
+            "Z": "--..",
+            "1": ".----",
+            "2": "..---",
+            "3": "...--",
+            "4": "....-",
+            "5": ".....",
+            "6": "-....",
+            "7": "--...",
+            "8": "---..",
+            "9": "----.",
+            "0": "-----",
+            ", ": "--..--",
+            ".": ".-.-.-",
+            "?": "..--..",
+            "!": "-.-.--",
+            "/": "-..-.",
+            "-": "-....-",
+            "(": "-.--.",
+            ")": "-.--.-",
+        }
+        self._characters = list(self._morse_chart.keys())
+        self._codes = list(self._morse_chart.values())
 
-    @classmethod
-    def _clean_morse(cls, morse: str) -> str:
-        """Spaces in the start, between, or end will be removed."""
+    @property
+    def morse_chart(self) -> dict[str, str]:
+        return self._morse_chart
 
-        # Create a list of morse without leading and trailing spaces.
-        clean_morse = [char for char in morse.split(" ") if char.strip()]
+    @property
+    def characters(self) -> list[str]:
+        return self._characters
 
-        # Join the morse with two spaces in between each element.
-        return "  ".join(clean_morse)
+    @property
+    def codes(self) -> list[str]:
+        return self._codes
 
-    @classmethod
-    def is_morse(cls, morse: str = None) -> bool:
-        """Checks if morse is valid."""
+    def parse_morse(self, morse: str) -> str:
+        """Removes leading, internal, and trailing spaces."""
+
+        if not morse.split():
+            return None
+
+        if not all(set(char.strip()) <= {".", "-"} for char in morse.split()):
+            return None
+
+        # NOTE: This will not return a formatted morse code in a sentence structure
+        # NOTE: rather it will return a sequence of morse codes separated with double spaces.
+        return "  ".join(morse.split())
+
+    def _has_dots_and_dashes(self, morse: str = None) -> bool:
+        """Parse and checks if morse contains valid symbols."""
+        # NOTE: As long as it only contains dots and dashes,
+        # doesn't matter the spacing, it will return True. Otherwise, False.
 
         try:
             if not morse.strip():  # Check if empty string.
                 return False
 
-            # If a set of character or a character is present in {".", "-"},
-            # then set(char) <= valid_symbols will evaluate to True.
-            return all(
-                set(char) <= {".", "-"} for char in cls._clean_morse(morse).split(" ")
-            )
+            # Split the given Morse code into individual characters.
+            # For each character in the split Morse code:
+            # Check if set(char) is a subset of the set {".", "-"}.
+            # If set(char) contains only the characters ".", or "-", it is considered as valid.
+            # If set(char) contains any other characters, it is considered as invalid.
+            # Return True if all set(char)s are valid, False otherwise.
+            return all(set(char.strip()) <= {".", "-"} for char in morse.split())
 
         except AttributeError:
             return False
 
-    @classmethod
-    def in_morse_chart(cls, char: str = None) -> tuple[str, str] | None:
-        """Checks if letter, number, symbol, or morse is in the chart."""
+    def in_morse_chart(self, char: str = None) -> tuple[str, str] | None:
+        """Checks if a letter, number, symbol, or morse is in the chart."""
 
         try:
             mod_char = char.strip().upper()
 
-            # Check if the input character is Morse code.
-            if cls.is_morse(char):
-                # If Morse code, return the corresponding character and code from the list.
+            if self._has_dots_and_dashes(char):
                 return (
-                    cls.characters[cls.codes.index(mod_char)],
-                    cls.codes[cls.codes.index(mod_char)],
+                    self.characters[self.codes.index(mod_char)],
+                    self.codes[self.codes.index(mod_char)],
                 )
 
-            # If not, return the corresponding character and code from the list.
             return (
-                cls.characters[cls.characters.index(mod_char)],
-                cls.morse_chart[mod_char],
+                self.characters[self.characters.index(mod_char)],
+                self.morse_chart[mod_char],
             )
 
         except (ValueError, TypeError, AttributeError):
             return None
 
-    @classmethod
-    def _update_chart(cls, char, morse):
-        cls.morse_chart.update({char.upper(): cls._clean_morse(morse)})
+    def _is_morse_in_chart(self, char: str, morse: str) -> bool:
+        """Checks if morse code or character is already in the chart."""
 
-        # Update each list to store the added item.
-        cls.characters = list(cls.morse_chart.keys())
-        cls.codes = list(cls.morse_chart.values())
+        return (
+            (char.upper(), morse.strip()) in self._morse_chart.items()
+            or morse.strip() in self._codes
+            or char.upper().strip() in self._characters
+        )
 
-    @classmethod
-    def add_morse(cls, char: str = None, morse: str = None):
+    def add_morse(self, char: str = None, morse: str = None):
         """Adds morse code to the chart."""
 
-        if not cls.is_morse(morse):
-            raise MorseCodeError("Invalid or no morse code specified.")
+        if not self._has_dots_and_dashes(morse):
+            raise ValueError("Invalid or no morse code specified.")
 
-        if (char.upper(), morse) in cls.morse_chart.items():
-            raise MorseCodeError("Morse code already in the chart.")
+        if self._is_morse_in_chart(char, morse):
+            raise ValueError("Morse code already in the chart.")
 
-        # Add to morse chart.
-        cls._update_chart(char, morse)
+        self._morse_chart.update({char.upper(): self.parse_morse(morse)})
+        self._characters = list(self._morse_chart.keys())
+        self._codes = list(self._morse_chart.values())
 
 
-class MorseEncrypt(Morse):
-    @classmethod
-    def encrypt_char(cls, char: str = None) -> str | None:
+class MorseEncrypt:
+    def __init__(self, morse) -> None:
+        self._morse = morse
+
+    def encrypt_char(self, char: str = None) -> str | None:
         """Encrypts a character."""
 
         try:
             return (
-                " " # Adds a trailing space.
+                " "  # Add a trailing space.
                 if char.strip() == ""
-                else cls.morse_chart.get(char.upper().strip()) + " "
+                else self._morse.morse_chart.get(char.upper().strip()) + " "
             )
 
         except (TypeError, ValueError):
             return None
 
-    @classmethod
-    def encrypt(cls, message: str = None) -> str | None:
+    def encrypt(self, message: str = None) -> str | None:
         """Encrypts a message."""
 
         morse_message = ""
-        morse_message_li = []  # For debugging white spaces issue.
 
         try:
             for char in message.upper():
-                morse_message += cls.encrypt_char(char)
-                morse_message_li.append(cls.encrypt_char(char))
+                morse_message += self.encrypt_char(char)
 
             return morse_message
 
@@ -160,32 +169,31 @@ class MorseEncrypt(Morse):
             return None
 
 
-class MorseDecrypt(Morse):
-    @classmethod
-    def decrypt_morse(cls, char: str = None) -> str | None:
+class MorseDecrypt:
+    def __init__(self, morse) -> None:
+        self._morse = morse
+
+    def decrypt_morse(self, char: str = None) -> str | None:
         """Decrypts a single morse code."""
 
         try:
             return (
-                " "  # Adds a trailing space.
+                " "  # Add a trailing space.
                 if char.strip() == ""
-                else cls.characters[cls.codes.index(char.strip())]
+                else self._morse.characters[self._morse.codes.index(char.strip())]
             )
 
         except (ValueError, AttributeError):
             return None
 
-    @classmethod
-    def decrypt(cls, message: str = None) -> str | None:
+    def decrypt(self, message: str = None) -> str | None:
         """Decrypts a message."""
 
         decrypted_message = ""
-        decrypted_message_li = []  # For debugging white spaces issue.
 
         try:
             for morse in message.split(" "):
-                decrypted_message += cls.decrypt_morse(morse)
-                decrypted_message_li.append(cls.decrypt_morse(morse))
+                decrypted_message += self.decrypt_morse(morse)
 
             return decrypted_message
 
@@ -194,15 +202,15 @@ class MorseDecrypt(Morse):
 
 
 def main():
-    Morse.add_morse("GUI", ".---...-")
-    print(f"In morse code chart: {Morse.in_morse_chart('GUI')}")
-    print(f"Encrypt: {MorseEncrypt.encrypt('HELLO World!')}")
-    print(
-        f"Decrypt: {MorseDecrypt.decrypt('.... . .-.. .-.. ---  --. .. - .... ..- -... -.-.--')}"
-    )
-    print(f"Encrypt character: {MorseEncrypt.encrypt_char('d')}")
-    print(f"Decrypt letter: {MorseDecrypt.decrypt_morse('-..-        ')}")
-    print(f"Is morse: {Morse.is_morse('--.  ---.  ---')}")
+    morse = Morse()
+    morse.add_morse("67", "-.----..-.---")
+    print(morse.in_morse_chart("67"))
+
+    encrypt = MorseEncrypt(morse)
+    print(encrypt.encrypt("This is bus"))
+
+    decrypt = MorseDecrypt(morse)
+    print(decrypt.decrypt("- .... .. ...  .. ...  -... ..- ..."))
 
 
 if __name__ == "__main__":
